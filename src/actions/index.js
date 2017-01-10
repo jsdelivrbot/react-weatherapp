0766cbf352f7b79eaa49da077ec15d82a5cfa5da
root <gerard.ramosm89@gmail.com>
2017-01-10 14:37:05 -0800

    axios request from weather api works

---
+++ src/actions/index.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+const API_KEY = '770aa7b6e360d1dd309e18e1195522d3';
+const ROOT_URL='http://api.openweathermap.org/data/2.5/forecast?appid=770aa7b6e360d1dd309e18e1195522d3';
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city){
+	const url=`${ROOT_URL}&q=${city},us`;
+	const request = axios.get(url);
+	return{
+		type: FETCH_WEATHER,
+		payload: request
+	};
+}
